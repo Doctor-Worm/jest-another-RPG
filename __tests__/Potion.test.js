f50e1cb4c9b074d9bcb24bca68ad4c2205d7b88b
Dr.Worm <sky.jackson@icloud.com>
2022-04-29 00:20:08 -0400

    updated tests

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,5 +1,9 @@
 const Potion = require('../lib/Potion');
 
+jest.mock('../lib/potion');
+
+console.log(new Potion());
+
 test('creates a health potion object', () => {
     const potion = new Potion('health');
 
